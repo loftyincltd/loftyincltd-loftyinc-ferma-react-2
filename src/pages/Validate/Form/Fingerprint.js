84a7f56b0fa61a92635a21b8dc5cee9c646c56bb
Odekunle Oluwafemi Michael <michaelfemi81@gmail.com>
2021-04-28 00:34:56 +0100

    newt

--- src/pages/Validate/Form/Fingerprint.js
+++ src/pages/Validate/Form/Fingerprint.js
@@ -40,6 +40,7 @@ reader.onSamplesAcquired = async (data) => {
   };
 
   return (
+    
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
       <button className={'btn'} style={{ width: '200px', margin: '0 auto', marginBottom: '30px', backgroundColor: 'green' }} onClick={click}>
         Reload Conection
